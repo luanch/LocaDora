2701a5062672a7eca0d39eba4433edc593a07d62
luanch <luan.borges@uniriotec.br>
2016-12-08 18:30:29 -0200

    visual do update filme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def ator_no_elenco elenco
     Elenco.find(elenco).ator.nome
   end
+
+  def listar_atores elencos
+    lista_atores = ''
+    elencos.each do |elenco|
+      lista_atores << ator_no_elenco(elenco) << ','
+    end
+    lista_atores.chomp(',')
+  end
 end
