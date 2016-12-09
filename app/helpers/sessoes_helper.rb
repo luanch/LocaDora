e376fc7f5a9214100f52967ae6b071165c88920c
luanch <luan.borges@uniriotec.br>
2016-12-09 19:11:44 -0200

    templates em todas as páginas

--- app/helpers/sessoes_helper.rb
+++ app/helpers/sessoes_helper.rb
@@ -3,12 +3,18 @@ module SessoesHelper
   def sign_in(pessoa, tipo = 2)
     p "!!!!!!!!!!!!!!!#{pessoa.id}"
     session[:email] = pessoa.email
-    @tipo_usuario = tipo
   end
 
-  def current_user
+  def current_user tipo=nil
     @current_user ||= Pessoa.find_by(email: session[:email])
+    return if(@current_user.nil?)
+    if(tipo == 1)
+        return 1 if Funcionario.find_by(pessoa_id: @current_user.id)
+        2 if Cliente.find_by(pessoa_id: @current_user.id)
+    end
+    @current_user
   end
+
 
   def block_access
     if @current_user.present?
@@ -23,11 +29,5 @@ module SessoesHelper
   def logout
     session.delete(:email)
     @current_user = nil
-  end
-
-  def tipo_usuario
-    return nil if @current_user.nil?
-    return 1 if Funcionario.find_by(pessoa_id: @current_user.id)
-    2 if Cliente.find_by(pessoa_id: @current_user.id)
   end
 end
